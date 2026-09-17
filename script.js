5fe4982988ea9d4ea41eaa0b70a3ee0f37247006
Nakarin Korsuwan <80184132+green-nk@users.noreply.github.com>
2026-09-17 07:15:14 +0000

    Add wrapper operate function

--- script.js
+++ script.js
@@ -2,3 +2,28 @@ const add = (a, b) => a + b;
 const subtract = (a, b) => a - b;
 const multiply = (a, b) => a * b;
 const divide = (a, b) => a / b;
+
+function operate(a, b, operator) {
+    let result;  
+
+    switch (operator) {
+        case '+':
+            result = add(a, b);
+            break;
+        case '-':
+            result = subtract(a, b);
+            break;
+        case '*':
+            result = multiply(a, b);
+            break;
+        case '/':
+            result = divide(a, b);
+            break;
+    }
+
+    return result;
+};
+
+let number;
+let otherNumber;
+let operator;
