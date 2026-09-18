9f3756edbfcf9737d86ee4484237830e9d27bef2
Nakarin Korsuwan <80184132+green-nk@users.noreply.github.com>
2026-09-18 07:11:22 +0000

    Display number and otherNumber

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 const OPERANDS = ['+', '-', '*', '/'];
 const FUNCTIONALS = ['=', 'c'];
-const INITIAL_DISPLAY = "-------------------";
 
 const add = (a, b) => a + b;
 const subtract = (a, b) => a - b;
@@ -23,6 +22,8 @@ function operate(a, b, operator) {
         case '/':
             result = divide(a, b);
             break;
+        default:
+            alert(`OOPS! No support for ${operator}`);
     }
 
     return result;
@@ -30,12 +31,12 @@ function operate(a, b, operator) {
 
 let number = "";
 let otherNumber = "";
-let operator;
+let operator = "";
 let isOperatorPressed = false;
 
 const displayContainer = document.querySelector(".display");
 const displayPara = document.createElement('p');
-displayPara.innerText = INITIAL_DISPLAY;
+displayPara.innerText = 0;
 
 displayContainer.appendChild(displayPara);
 
@@ -44,12 +45,12 @@ for (let i = 0; i < 10; i++) {
     const btn = document.createElement("button");
     btn.innerText = i;
     btn.addEventListener("click", () => {
+        const displayPara = document.querySelector(".display p");
+        if (!isOperatorPressed && !number || isOperatorPressed && !otherNumber) displayPara.innerText = "";
+        displayPara.innerText += btn.innerText;
+
         if (!isOperatorPressed) number += btn.innerText;
         else otherNumber += btn.innerText;
-
-        const displayPara = document.querySelector(".display p");
-        if (displayPara.textContent == INITIAL_DISPLAY) displayPara.innerText = "";
-        displayPara.innerText += btn.innerText;
     });
 
     digitContainer.appendChild(btn);
